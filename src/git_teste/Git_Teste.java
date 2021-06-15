cec802682a2edd59c416c694863a877717dd64d7
Lucas Bernardes <lbeurncaradess@protonmail.com>
2021-06-15 19:49:46 -0300

    Git_Teste.java: modificação

--- src/git_teste/Git_Teste.java
+++ src/git_teste/Git_Teste.java
@@ -5,6 +5,7 @@ public class Git_Teste {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println("Mais uma linha");
+        System.out.println("Ok mais uma vez");
     }
     
 }
