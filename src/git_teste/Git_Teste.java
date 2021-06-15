e0cb2df56b0c716a43109f8857f320fd4a2dd16e
Lucas Bernardes <lbeurncaradess@protonmail.com>
2021-06-15 20:06:52 -0300

    Atualizado branch_variaveis Git_Teste.java: Criando variavel estática de idade

--- src/git_teste/Git_Teste.java
+++ src/git_teste/Git_Teste.java
@@ -3,9 +3,13 @@ package git_teste;
 public class Git_Teste {
 
     public static void main(String[] args) {
+        
+        int idade = 25;
+        
         System.out.println("Hello Word");
         System.out.println("Mais uma linha");
         System.out.println("Ok mais uma vez");
+        System.out.println("Você tem " + 25 + "anos.");
     }
     
 }
